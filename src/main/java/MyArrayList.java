978b8d5dff3b3a1dcc431b257e03758b6dae0d96
ant <anthonyjones@zipcoder3.home>
2017-05-19 20:07:47 -0400

    First Commit

---
+++ src/main/java/MyArrayList.java
@@ -0,0 +1,50 @@
+/**
+ * Created by anthonyjones on 5/19/17.
+ */
+public class MyArrayList<E> {
+
+    MyArrayList() {
+    }
+
+    public MyArrayList(int index) {
+    }
+
+    public boolean add(E e) {
+        return false;
+    }
+
+    public E remove(int index) {
+        return null;
+    }
+
+    public boolean remove(Object o) {
+        return false;
+    }
+
+    public E get(int index) {
+        return null;
+    }
+
+    public E set(int index, E element) {
+        return null;
+    }
+
+    public boolean isEmpty() {
+        return false;
+    }
+
+    public boolean contains() {
+        return false;
+    }
+
+    public int size() {
+        return 0;
+    }
+
+    public void resizer() {
+    }
+
+
+
+
+}
---
+++ src/test/java/testMyArrayList.java
@@ -0,0 +1,37 @@
+/**
+ * Created by anthonyjones on 5/19/17.
+ */
+public class testMyArrayList {
+
+    public void add() {
+
+    }
+
+    public void remove() {
+
+    }
+
+    public void get(int index) {
+
+    }
+
+    public void set() {
+
+    }
+
+    public void isEmpty() {
+
+    }
+
+    public void contains() {
+
+    }
+
+    public void size() {
+
+    }
+
+
+
+
+}
